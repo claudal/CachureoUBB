2800284c6b802f5a8502ba891742a85b6e6924fb
claudal <claudio.gaete2301@alumnos.ubiobio.cl>
2026-09-25 20:15:55 -0300

    feat: agrega filtro de marcado por rut de encargado para la lista de alertas de búsqueda

--- backend/src/schemas/alertaBusqueda.schema.js
+++ backend/src/schemas/alertaBusqueda.schema.js
@@ -15,5 +15,6 @@ export const updateAnimalSchema = createAnimalSchema.partial();*/
 
 export const filtroAlertaBusquedaSchema = z.object({
   id_estado: z.string().regex(/^\d+$/).transform(Number).optional(),
-  rut_autor: z.string().refine((rut) => rutjs.validate(rut),{error: "El rut del autor debe ser válido"}).optional()
+  rut_autor: z.string().refine((rut) => rutjs.validate(rut),{error: "El rut del autor debe ser válido"}).optional(),
+  rut_marcador: z.string().refine((rut) => rutjs.validate(rut),{error: "El rut del encargado en el marcador debe ser válido"}).optional()
 });
--- backend/src/services/alertaBusqueda.services.js
+++ backend/src/services/alertaBusqueda.services.js
@@ -1,8 +1,17 @@
 import { prisma } from "../config/prisma.js";
 
 export function listar(filtros = {}){
+    /**@type {import("@prisma/client").Prisma.AlertaBusquedaWhereInput}*/
     const where = {};
     if(filtros.id_estado) where.id_estado = filtros.id_estado;
     if(filtros.rut_autor) where.rut_autor = filtros.rut_autor;
-    return prisma.alertaBusqueda.findMany({where});
+    if(filtros.rut_encargado) {
+        if(filtros.marcado) 
+            where.marcadores = {
+                some: {
+                    rut_encargado: filtros.rut_marcador
+                }
+            };
+    }
+    return prisma.alertaBusqueda.findMany({where,include});
 }
