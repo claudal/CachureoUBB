dd9d27c25ffd135fb3a6a69deba9bcd5b352aa4e
claudal <claudio.gaete2301@alumnos.ubiobio.cl>
2026-09-25 17:15:09 -0300

    feat: agrega validador de filtro en GET de alertaBusqueda

--- backend/src/routes/alertaBusqueda.routes.js
+++ backend/src/routes/alertaBusqueda.routes.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import { getAlertasBusqueda } from "../controllers/alertaBusqueda.controller.js";
+import { validate } from "../middlewares/validador.middleware.js";
+import { filtroAlertaBusquedaSchema } from "../schemas/alertaBusqueda.schema.js";
 
 const router = Router();
 
-router.get('/',getAlertasBusqueda);
+router.get('/',validate(filtroAlertaBusquedaSchema, 'query'), getAlertasBusqueda);
 
 export default router;
---
+++ backend/src/schemas/alertaBusqueda.schema.js
@@ -0,0 +1,17 @@
+import { z } from 'zod';
+
+/*TODO
+export const createAnimalSchema = z.object({
+  nombre: z.string().min(2, 'El nombre es obligatorio').max(100),
+  edad: z.number().int().nonnegative('La edad no puede ser negativa'),
+  peso: z.number().positive().optional(),
+  disponible: z.boolean().default(true),
+  especieId: z.number().int().positive('Debe indicar una especie válida'),
+  recintoId: z.number().int().positive('Debe indicar un recinto válido'),
+});
+
+export const updateAnimalSchema = createAnimalSchema.partial();*/
+
+export const filtroAlertaBusquedaSchema = z.object({
+  id_estado: z.string().regex(/^\d+$/).transform(Number).optional()
+});
