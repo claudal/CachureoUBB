df684e33303d198d0369d58c9b64be656544a72a
claudal <claudio.gaete2301@alumnos.ubiobio.cl>
2026-09-25 12:35:22 -0300

    test: elimina petición GET de prueba

--- backend/src/controllers/test.js
+++
@@ -1,3 +0,0 @@
-export async function funcionPrueba(req, res, next){
-    res.json({"mensaje":"hola mundo"});
-}
--- backend/src/routes/index.routes.js
+++ backend/src/routes/index.routes.js
@@ -1,6 +1,5 @@
 import { Router } from "express";
-import { funcionPrueba } from "../controllers/test.js";
 
 export const router = Router();
 
-router.get("/test/",funcionPrueba);
+//router.use("/usuario",funcionPrueba);
