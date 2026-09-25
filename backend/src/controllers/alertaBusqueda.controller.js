e3243a2fb776714a27c442aac00a6f3c317bcfef
claudal <claudio.gaete2301@alumnos.ubiobio.cl>
2026-09-25 20:38:46 -0300

    docs: agrega comentarios JSDoc para funciones que tienen de parámetro req, res y next

--- backend/src/controllers/alertaBusqueda.controller.js
+++ backend/src/controllers/alertaBusqueda.controller.js
@@ -1,5 +1,11 @@
 import * as services from "../services/alertaBusqueda.services.js";
 
+/**
+ * 
+ * @param {import("express").Request} req 
+ * @param {import("express").Response} res 
+ * @param {import("express").NextFunction} next 
+ */
 export async function getAlertasBusqueda(req, res, next) {
     try { 
         res.json(await services.listar(req.vQuery));
